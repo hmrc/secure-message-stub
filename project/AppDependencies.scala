c6a276aaf913645fa8600687c02da67083b3d3da
Andrew Walker <154334+ardwalker@users.noreply.github.com>
2025-06-25 08:05:46 +0100

    DC-7536: PR Commenter issues

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "9.13.0"
   private val frontendPlayVersion = "10.13.0"
   
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % frontendPlayVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.0.0",
+    "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.1.0",
     "com.beachape"      %% "enumeratum"                 % "1.7.4",
     "commons-codec"     %  "commons-codec"              % "1.15"
   )
