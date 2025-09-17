fb27c48c230c6e34212b11994917550af121d1f2
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2025-09-17 09:53:19 +0100

    DC-7799 Remove enumeratm lib dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,6 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % frontendPlayVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.1.0",
-    "com.beachape"      %% "enumeratum"                 % "1.7.4",
     "commons-codec"     %  "commons-codec"              % "1.15"
   )
 
