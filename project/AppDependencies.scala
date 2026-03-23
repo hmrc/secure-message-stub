5dfbe397dae5f110af37746ea5ada625d6f2bb25
Vineet Tyagi <22053543+tyagikumarvineet@users.noreply.github.com>
2026-03-23 12:36:20 +0000

    [DC-8629] Update library versions to fix vulnerabilities reported by Platform team (#99)
    * [DC-8629] Update library versions to fix vulnerabilities with IDs CVE-2025-66566 and CVE-2025-12183
    
    * [DC-8629] Update scala version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,23 +1,23 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapVersion = "10.5.0"
-  private val frontendPlayVersion = "12.25.0"
-  
+  private val bootstrapVersion = "10.7.0"
+  private val frontendPlayVersion = "12.32.0"
+
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % frontendPlayVersion,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-partials-play-30"      % "10.2.0",
-    "commons-codec"     %  "commons-codec"              % "1.20.0"
+    "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30" % frontendPlayVersion,
+    "uk.gov.hmrc"  %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"  %% "play-partials-play-30"      % "10.2.0",
+    "commons-codec" % "commons-codec"              % "1.20.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "org.jsoup"              %  "jsoup"                   % "1.21.2",
-    "org.playframework"      %% "play-test"               % PlayVersion.current,
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
+    "org.jsoup"          % "jsoup"                  % "1.21.2",
+    "org.playframework" %% "play-test"              % PlayVersion.current
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
